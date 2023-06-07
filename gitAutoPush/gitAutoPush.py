a37a7acf7e8cdee7441f4644737bc4ccbf1c1cc4
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-07 20:57:41 +0900

    -- 2023-06-07 20:57에 자동업데이트 함 --

--- gitAutoPush/gitAutoPush.py
+++ gitAutoPush/gitAutoPush.py
@@ -39,29 +39,30 @@ os.chdir(local_repo)
 # 원격 저장소를 추가합니다.
 subprocess.call(["git", "remote", "add", "origin", remote_url])
 
-#while True:
+while True:
 
-# 한국 타임존을 설정합니다.
-# 형식에 맞게 현재 시간을 얻습니다.
-formatted_time = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y-%m-%d %H:%M")
+    # 한국 타임존을 설정합니다.
+    # 형식에 맞게 현재 시간을 얻습니다.
+    formatted_time = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y-%m-%d %H:%M")
 
-# 원격 저장소의 변경 사항을 가져옵니다.
-subprocess.run(["git", "pull", "origin", "main"])
+    # 원격 저장소의 변경 사항을 가져옵니다.
+    subprocess.run(["git", "pull", "origin", "main"])
 
-# git status
-result = subprocess.run(["git", "status"], capture_output=True, text=True)
+    # git status
+    result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
-# git add --all
-subprocess.run(["git", "add", "."])
+    # git add --all
+    subprocess.run(["git", "add", "."])
 
-# 변경된 파일의 수
-num_files = len(result.stdout.splitlines()) - 1
+    # 변경된 파일의 수
+    num_files = len(result.stdout.splitlines()) - 1
 
-# 변경된 파일이 있는 경우 커밋하고 푸시합니다.
-if num_files > 0:
-    subprocess.run(["git", "commit", "-m", f"-- {formatted_time}에 자동업데이트 함 --"])
-    subprocess.run(["git", "push", "origin", "main"])
+    # 변경된 파일이 있는 경우 커밋하고 푸시합니다.
+    if num_files > 0:
+        subprocess.run(["git", "commit", "-m", f"-- {formatted_time}에 자동업데이트 함 --"])
+        subprocess.run(["git", "push", "origin", "main"])
 
 
-# 10 초 동안 대기합니다.
+    # 10 초 동안 대기합니다.
     # time.sleep(10)
+    break;
