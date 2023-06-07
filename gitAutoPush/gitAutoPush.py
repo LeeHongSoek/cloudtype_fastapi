4da701e1370a5f51f525392ded063ff958f1eb92
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-07 22:30:38 +0900

    -- 수정  2023 / 06 / 07  22 : 30 --

--- gitAutoPush/gitAutoPush.py
+++ gitAutoPush/gitAutoPush.py
@@ -43,7 +43,7 @@ while True:
 
     # 한국 타임존을 설정합니다.
     # 형식에 맞게 현재 시간을 얻습니다.
-    formatted_time = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y - %m - %d  %H : %M")
+    formatted_time = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y / %m / %d  %H : %M")
 
     # 원격 저장소의 변경 사항을 가져옵니다.
     subprocess.run(["git", "pull", "origin", "main"])
@@ -59,7 +59,7 @@ while True:
 
     # 변경된 파일이 있는 경우 커밋하고 푸시합니다.
     if num_files > 0:
-        subprocess.run(["git", "commit", "-m", f"-- 수정 : {formatted_time} --"])
+        subprocess.run(["git", "commit", "-m", f"-- 수정  {formatted_time} --"])
         subprocess.run(["git", "push", "origin", "main"])
 
 
