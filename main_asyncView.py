c62a8cf09b655efc2453afc017d3335f8291af23
이홍석(GMail) <lhs0806@gmail.com>
2023-06-07 13:42:15 +0900

    rename

--- main_asyncView.py
+++ main_asyncView.py
@@ -7,7 +7,7 @@ long_running_task 함수는 작업의 진행 상태를 지속적으로 업데이
 여기서는 1초마다 업데이트를 수행하도록 하였습니다. 
 작업이 완료되면 "작업 완료!" 메시지를 클라이언트로 전송합니다.
 
-run_websocket 함수는 WebSocket 연결을 수락하고, 작업 상태 업데이트를 위해 
+run_long_running_task 함수는 WebSocket 연결을 수락하고, 작업 상태 업데이트를 위해 
 long_running_task를 비동기 작업으로 실행합니다. 
 작업이 완료될 때까지 대기하고, 클라이언트가 연결을 끊으면 작업을 취소합니다.
 
@@ -17,7 +17,7 @@ long_running_task를 비동기 작업으로 실행합니다.
 이 예제는 WebSocket을 사용하여 작업 상태를 업데이트하고 클라이언트에 전달하는 방법을 보여줍니다. 
 필요에 따라 작업 상태를 데이터베이스에 저장하거나 다른 방법으로 전달할 수도 있습니다.
 
-uvicorn FastApi_asyncView:app --reload  --host=0.0.0.0 --port=10000
+uvicorn main_asyncView:app --reload  --host=0.0.0.0 --port=10000
 taskkill /f /im python.exe         프로세서를 완전히 죽이고 하자.. 씨벌 좃같네
 
 pip install fastapi
@@ -57,8 +57,8 @@ def home_asyncView(request: Request):
 
 
 
-@app.websocket("/task")
-async def run_websocket(websocket: WebSocket):
+@app.websocket("/long_running_task")
+async def run_long_running_task(websocket: WebSocket):
     await websocket.accept()
 
     # 작업 상태 업데이트를 위한 비동기 작업 시작
