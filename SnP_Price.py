bf5a9744b76af6886c16ae6559d8274fe7f211f9
이홍석(GMail) <lhs0806@gmail.com>
2023-06-12 14:14:03 +0900

    -- 수정  2023 / 06 / 12  14 : 14 --

--- SnP_Price.py
+++ SnP_Price.py
@@ -24,16 +24,22 @@ for index, row in sp500.iterrows():
     symbol = row['Symbol']
     company_name = row['Name']
 
-    days = -12
+    days = -20
 
     try:
         # 최신 종가와 시가 가져오기
         data = yf.download(symbol, period='max')
-        prices = data.iloc[days:]  # 최근부터 -12일까지의 데이터 선택
+        prices = data.iloc[days:]  # 최근부터 -20일까지의 데이터 선택
 
         for i in range(len(prices)):
             close, open, date = prices['Close'].iloc[i], prices['Open'].iloc[i], prices.index[i].strftime('%Y-%m-%d')
             change_rate = (close - open) / open * 100
+
+            # 5일 평균 계산
+            avg_5 = prices['Close'].iloc[i-4:i+1].mean()
+
+            # 20일 평균 계산
+            avg_20 = prices['Close'].iloc[i-19:i+1].mean()
 
             # 종목 정보 저장
             cursor.execute("INSERT INTO sp500_stocks (symbol, company_name) VALUES (%s, %s) "
@@ -41,14 +47,15 @@ for index, row in sp500.iterrows():
                            (symbol, company_name))
 
             # 일자별 데이터 저장
-            cursor.execute("INSERT INTO stock_prices (symbol, date, open, close, change_rate) "
-                           "VALUES (%s, %s, %s, %s, %s) "
+            cursor.execute("INSERT INTO stock_prices (symbol, date, open, close, change_rate, avg_5, avg_20) "
+                           "VALUES (%s, %s, %s, %s, %s, %s, %s) "
                            "ON DUPLICATE KEY UPDATE open = VALUES(open), close = VALUES(close), "
-                           "change_rate = VALUES(change_rate)",
-                           (symbol, date, open, close, change_rate))
+                           "change_rate = VALUES(change_rate), avg_5 = VALUES(avg_5), avg_20 = VALUES(avg_20)",
+                           (symbol, date, open, close, change_rate, avg_5, avg_20))
 
             # 데이터 출력
-            print("Symbol:", symbol, "| Date:", date, "| Open:", open, "| Close:", close, "| Change Rate:", change_rate)
+            print("Symbol:", symbol, "| Date:", date, "| Open:", open, "| Close:", close,
+                  "| Change Rate:", change_rate, "| 5-day Avg:", avg_5, "| 20-day Avg:", avg_20)
 
         conn.commit()
         print(f"Data saved for Symbol: {symbol} | Company: {company_name}")
