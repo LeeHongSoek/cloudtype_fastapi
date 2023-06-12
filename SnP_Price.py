d10ca9208aa25d37440976f4645eece50f4557c8
이홍석(GMail) <lhs0806@gmail.com>
2023-06-12 13:24:44 +0900

    -- 수정  2023 / 06 / 12  13 : 24 --

--- SnP_Price.py
+++ SnP_Price.py
@@ -1,20 +1,26 @@
 import pandas as pd
 import requests
+import io
+import yfinance as yf
 
 # S&P 500 종목 가져오기
 url = "https://datahub.io/core/s-and-p-500-companies/r/constituents.csv"
 data = requests.get(url).content
-sp500 = pd.read_csv(pd.compat.StringIO(data.decode('utf-8')))
+sp500 = pd.read_csv(io.StringIO(data.decode('utf-8')))
 
 # 각 종목의 시가와 종가 가져오기
-for symbol in sp500['Symbol']:
-    # 최신 종가와 시가 가져오기
-    data = yf.download(symbol, period='1d')
-    latest_close = data['Close'].iloc[-1]
-    latest_open = data['Open'].iloc[-1]
+for index, row in sp500.iterrows():
+    symbol = row['Symbol']
+    company_name = row['Name']
+    
+    try:
+        # 최신 종가와 시가 가져오기
+        data = yf.download(symbol, period='1d')
+        latest_close = data['Close'].iloc[-1]
+        latest_open = data['Open'].iloc[-1]
 
-    print("Symbol:", symbol)
-    print("Date:", latest_close.name.strftime("%Y-%m-%d"))
-    print("Latest Close:", latest_close)
-    print("Latest Open:", latest_open)
+        print(f"{index+1}. Symbol: {symbol} | Company: {company_name} | Date: {data.index[-1].strftime('%Y-%m-%d')} | Latest Close: {latest_close} | Latest Open: {latest_open}")
+    except Exception as e:
+        print("Error occurred while fetching data for symbol:", symbol)
+        print("Error message:", str(e))
     print()
