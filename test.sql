987ac791a2da8eec98594b770245bd79da3d707f
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-16 00:52:40 +0900

    -- 2023 / 06 / 16  00 : 52 --

--- test.sql
+++ test.sql
@@ -1,3 +1,29 @@
+ SELECT m.symbol,
+           m.tr_date,
+           m.close,
+           u.crossing AS uc,
+           d.crossing AS dc
+    FROM stock_prices m
+    LEFT JOIN (
+        SELECT symbol,
+               tr_date,
+               close,
+               crossing
+        FROM stock_prices
+        WHERE crossing IN ('[U]p')
+    ) u ON m.symbol = u.symbol
+    AND m.tr_date = u.tr_date
+    LEFT JOIN (
+        SELECT symbol,
+               tr_date,
+               close,
+               crossing
+        FROM stock_prices
+        WHERE crossing IN ('[D]own')
+    ) d ON m.symbol = d.symbol
+    AND m.tr_date = d.tr_date
+
+;    
 select *
 from (
         SELECT CASE
