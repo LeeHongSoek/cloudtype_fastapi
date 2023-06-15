f34c410db266e74f7825ba28268ef60e7e2f274d
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-16 00:26:35 +0900

    -- 2023 / 06 / 16  00 : 26 --

--- test.sql
+++ test.sql
@@ -1,31 +1,36 @@
-
-
-
-SELECT distinct m.symbol
-  from stock_prices m
-  
-;
-
-SELECT *
-FROM
-(
-SELECT m.symbol, m.tr_date, m.close , u.crossing uc, d.crossing dc
-  from stock_prices m
-left join   
-(
-SELECT symbol, tr_date, close, crossing from stock_prices
-where crossing in ('[U]p')
-) u
-on m.symbol = u.symbol 
-and m.tr_date = u.tr_date
-left join   
-(
-SELECT symbol, tr_date, close, crossing from stock_prices
-where crossing in ('[D]own')
-) d
-on m.symbol = d.symbol 
-and m.tr_date = d.tr_date
-)
-where uc is not null or  dc is not null 
-ORDER by 1,2
-;
+select *
+from (
+        SELECT CASE
+                WHEN ROW_NUMBER() OVER (
+                    PARTITION BY m.symbol
+                    ORDER BY m.tr_date
+                ) = 1 THEN m.symbol
+                ELSE ''
+            END AS symbol,
+            m.tr_date,
+            m.close,
+            ifnull(u.crossing, '') AS uc,
+            ifnull(d.crossing, '') AS dc
+        FROM stock_prices m
+            LEFT JOIN (
+                SELECT symbol,
+                    tr_date,
+                    close,
+                    crossing
+                FROM stock_prices
+                WHERE crossing IN ('[U]p')
+            ) u ON m.symbol = u.symbol
+            AND m.tr_date = u.tr_date
+            LEFT JOIN (
+                SELECT symbol,
+                    tr_date,
+                    close,
+                    crossing
+                FROM stock_prices
+                WHERE crossing IN ('[D]own')
+            ) d ON m.symbol = d.symbol
+            AND m.tr_date = d.tr_date
+    )
+where symbol <> ''
+    or uc <> ''
+    or dc <> ''
