23fd858ccd7dd4b0d07a6be17deff2d79c53cac0
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-06-15 21:58:47 +0900

    -- 수정  2023 / 06 / 15  21 : 58 --

--- Snp_Subfunc.py
+++ Snp_Subfunc.py
@@ -180,7 +180,6 @@ def fetch_store_stock_prices(conn, cursor, symbol, company_name, days):
             print(f"{i+1:3d} | {date} |시: {open_:>4.5f} |종: {close_:>4.5f} |률: {sign}{abs(change_rate):>3.4f} |량: {volume_:8d} |5/20: {avg_5} {compare} {avg_20} | /{crossing_}/")
 
 
-
         query = ''' UPDATE sp500_stocks
                        SET date_update = datetime('now')
                      WHERE symbol       = ?
