876f84d7e6543bf45955293dd4b258b8f9c5c9d8
이홍석(GMail) <lhs0806@gmail.com>
2023-07-11 20:07:49 +0900

    [ 2023 / 07 / 11   20 : 07 ]

--- wf_testJob/wf_testJob.py
+++ wf_testJob/wf_testJob.py
@@ -3,6 +3,6 @@ import pytz
 
 timezone = pytz.timezone('Asia/Seoul')
 
-with open('file.txt', 'w') as f:
+with open('wf_testJob.txt', 'w') as f:
     f.write(f"현재 한국의 작업시간은 : {datetime.now(timezone)} 입니다.")
 
